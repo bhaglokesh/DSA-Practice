70884fee1350c8e9ca17a9618b46caf172508f11
bhaglokesh <bhag.loki@gmail.com>
2026-03-12 14:10:08 +0530

    Reverse an darray even odd

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+//logic to reverse even adn odd array
+
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<"Printing DONE"<<endl;
+}
+
+
+void reverseArray(int arr[], int n){
+   int start =0;
+   int end = n-1;
+
+   while(start<=end){
+     swap(arr[start],arr[end]);
+     start++;   
+     end--;
+   }
+}
+
+int main(){
+
+    int arr[6] = {1,4,0,5,-2,15};
+    int brr[5] = {2,6,3,9,4};
+
+    reverseArray(arr,6);
+    reverseArray(brr,5);
+
+    printArray(arr,6);
+    printArray(brr,5);
+
+    return 0;
+
+
+
+    // int n = sizeof(arr)/sizeof(int);
+
+
+}
