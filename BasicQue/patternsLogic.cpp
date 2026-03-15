d93d206955455b9bd8795342d65fc7f2ada7ae16
bhaglokesh <bhag.loki@gmail.com>
2026-03-15 18:56:26 +0530

    Hollow star

--- BasicQue/patternsLogic.cpp
+++ BasicQue/patternsLogic.cpp
@@ -1,23 +1,66 @@
 #include<iostream>
 using namespace std;
 
+//solid rectangle pattern
 void pattern1(){
     //solid rectangle pattern
     // * * * * * 
     // * * * * * 
     // * * * * *
-
-    for(int i=0;i<3;i++){
-        for(int j =0; j<5;j++){
+    
+    //row - observation
+    for(int row=0;row<3;row++){
+        //col - observation
+        for(int col =0; col<5;col++){
             cout <<"* ";
         }
         cout << endl; //after each row print a new line
     }
 }
 
+//Hollow rectangle pattern
+void pattern2(){
+    //Hollow rectangle pattern
+    // * * * * * 
+    // *       * 
+    // * * * * *
+
+    // //outer loop for rows
+    // for(int i=0;i<3;i++){
+    //     //inner loop for columns
+    //     for(int j=0;j<5;j++){
+    //         if(i==0 || i==2 || j==0 || j==4){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    //another approach
+    for(int row =0; row< 3;row++){
+        if(row == 0 || row ==2){
+            for(int col =0; col<5;col++){
+                cout<<"* ";
+            }
+        }
+        else{
+           cout << "*";
+              for(int col =0; col<3;col++){
+                 cout<<"  ";
+                }
+                cout<<" * ";
+        }
+        cout<<endl;
+    }
+
+}
+
 int main(){
 
-    pattern1();
+    pattern2();
 
 
 
