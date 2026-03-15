1f6a33487ed6de8d1e145ab51ca914239ed9e272
bhaglokesh <bhag.loki@gmail.com>
2026-03-15 18:20:28 +0530

    Basic Conditional i/p o/p

---
+++ BasicQue/basicConditional.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // famous interview question to check if the input is taken or not
+    int n;
+    if(cin>>n){
+        cout<< "Lokesh" << endl;
+    }
+
+
+    if(cout<<"Lokesh"){
+        cout<<"Bhagadkar"<<endl;
+    }
+
+
+    return 0;
+}
