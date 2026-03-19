e6dc70034036bc8c6bad95adcf6a9301d9874630
bhaglokesh <bhag.loki@gmail.com>
2026-03-19 22:19:30 +0530

    2D array access

---
+++ Array/2Darray.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+//2D array
+
+int main(){
+
+    int arr[3][4] = {{1,2,3,4},
+                     {5,6,7,8},
+                     {9,10,11,12}};
+
+
+
+    //accessing rowwise
+    cout<<"Rowwise: "<<endl;
+    for(int i = 0; i< 3; i++){
+        for(int j = 0; j< 4; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    //accessing columnwise 
+    cout<<"Columnwise: "<<endl;
+     for(int i = 0; i< 3; i++){
+        for(int j = 0; j< 4; j++){
+            cout<<arr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
