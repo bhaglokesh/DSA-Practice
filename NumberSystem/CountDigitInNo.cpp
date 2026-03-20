7c64304ec92fbcfd8a31c4ded087ad379e47ad5a
bhaglokesh <bhag.loki@gmail.com>
2026-03-20 19:33:17 +0530

    Counting No of Digits logic

---
+++ NumberSystem/CountDigitInNo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//https://www.geeksforgeeks.org/problems/count-digits-1606889545/1
+
+
+int main(){
+    int  n;
+    int count = 0;
+    cin >> n;
+  
+    while(n>0){
+        int lastdigit = n%10;
+        count++;
+        n = n/10;
+    }
+    cout<< "The No od digits :"<<count;
+    return 0;
+}
