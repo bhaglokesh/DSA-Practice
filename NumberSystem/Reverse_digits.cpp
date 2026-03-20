10f580edb1192e07bc3af2e9788cb5d01ea72c02
bhaglokesh <bhag.loki@gmail.com>
2026-03-20 20:05:46 +0530

    Reverse a number

---
+++ NumberSystem/Reverse_digits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//https://www.geeksforgeeks.org/problems/reverse-digit0316/1
+
+int main(){
+
+    int ans =0;
+    int revNum = 0;
+    int n;
+    cin >> n;
+    
+    while(n>0){
+        int ld = n%10;
+        ans = ans*10 + ld;
+        n = n/10;
+    }
+    cout << "reverse No: "<<ans; 
+    return 0;
+        
+}
