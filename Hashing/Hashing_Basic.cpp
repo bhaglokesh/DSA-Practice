7c757cb57152fa6dc60c26763a4bce79bdeae0a0
bhaglokesh <bhag.loki@gmail.com>
2026-03-22 15:06:30 +0530

    Basic Hashing Implementation to Count No of occurance

---
+++ Hashing/Hashing_Basic.cpp
@@ -0,0 +1,46 @@
+/*
+🧠 Problem: Hashing_Basic
+
+💡 Key Idea:
+- we use another array to stor count of no of 
+occurance of element
+
+🚀 Approach:
+- create another arry 
+- use counter as idea for array
+
+⚠️ Edge Cases:
+- 
+
+⏱ TC:
+📦 SC:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    //precompute
+    int hash[13] ={0};
+    for(int i =0; i<n;i++){
+        hash[arr[i]] +=1;
+    }
+
+    int q;
+    cin >> q;
+    while(q--){
+        int number;
+        cin >> number;
+        //fetch
+        cout << hash[number] << endl;
+    }
+    
+    return 0;
+}
---
+++ Pattern/patternMaster.cpp
@@ -0,0 +1,17 @@
+/*
+🧠 Problem: patternMaster
+
+🚀 Approach:
+- 
+
+⏱ Time Complexity:
+📦 Space Complexity:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
